5ff8a45138569c95960cea3405e485325042d78f
Gramajo M. Benjamin <gramajomatiasbenjamin14@gmail.com>
2026-05-30 18:13:37 -0300

    Terminar la página de destacados junto con sus estilos CSS y lógica JS

---
+++ destacados.js
@@ -0,0 +1,12 @@
+const tarjetas = document.querySelectorAll(".tarjeta");
+const contador = document.getElementById("contador");
+
+tarjetas.forEach((tarjeta) => {
+  tarjeta.addEventListener("click", () => {
+    tarjeta.classList.toggle("seleccionada");
+
+    const seleccionadas = document.querySelectorAll(".seleccionada").length;
+
+    contador.textContent = `Seleccionadas: ${seleccionadas}`;
+  });
+});
